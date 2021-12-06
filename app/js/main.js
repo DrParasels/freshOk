ab0ffbb37b5b87b24a423da51f393d178a70a547
DrParasels <fomishov121@yandex.ru>
2021-12-06 13:17:18 +0300

    add 2 mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,17 @@ $(function(){
    
   });
 
-  var mixer = mixitup('.top-products');
+  var containerEl1 = document.querySelector('[data-ref="top-products-mix"]');
+  var containerEl2 = document.querySelector('[data-ref="promo-mix"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
 });
+
